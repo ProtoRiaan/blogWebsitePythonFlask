18b47644e6f79c10cdbd888d03660de9672fb6d2
Riaan Schuld <rhschuld@gmail.com>
2023-04-02 05:54:16 +0000

    added some test data and basic framework for the blog page

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Riaan Schuld',
+        'title': 'Blogpost Test1',
+        'content': 'Test for post 1',
+        'datePosted' : 'April 2nd 2023'
+    },
+    {
+        'author': 'Diana Collins',
+        'title': 'Blogpost Test2',
+        'content': 'Test for post 2',
+        'datePosted' : 'April 2nd 2023'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def Home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def About():
+    return render_template('about.html')
+
+@app.route("/blog")
+def Blog():
+    return render_template('blog.html', posts=posts)
 
 
 
