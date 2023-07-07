3c536d77f2f56198656244dbd98f4bbcffae2513
Riaan Schuld <rhschuld@gmail.com>
2023-07-07 11:21:04 +0000

    added error pages and routes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -39,8 +39,10 @@ def create_app(config_class=Config):
   from flaskblog.users.views import users
   from flaskblog.posts.views import posts
   from flaskblog.main.views import main
+  from flaskblog.errors.handlers import errors
   app.register_blueprint(users)
   app.register_blueprint(posts)
   app.register_blueprint(main)
+  app.register_blueprint(errors)
 
   return app
