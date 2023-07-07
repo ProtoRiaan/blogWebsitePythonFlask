d6062f87bdd59513779d60e37390e54f6aa8b599
Riaan Schuld <rhschuld@gmail.com>
2023-07-07 20:23:45 +0000

    added DB migration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -11,6 +12,8 @@ from flaskblog.config import Config
 
   #instantiate the DB
 db = SQLAlchemy()
+migrate = Migrate()
+
 
   #instantiating password hashing  
 bcrypt = Bcrypt()
@@ -26,7 +29,7 @@ mail = Mail()
 
 
 
- #instantiate the flask instance 
+ #instantiate the flask app instance 
 def create_app(config_class=Config):
   app = Flask(__name__)
   app.config.from_object(Config)
@@ -35,6 +38,7 @@ def create_app(config_class=Config):
   bcrypt.init_app(app)
   login_manager.init_app(app)
   mail.init_app(app)
+  migrate.init_app(app, db)
 
   from flaskblog.users.views import users
   from flaskblog.posts.views import posts
