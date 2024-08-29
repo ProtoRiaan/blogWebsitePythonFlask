4008d295999140361d2783b081c329a1f2c14a2d
Riaan Schuld <rhschuld@gmail.com>
2024-08-29 09:17:08 -0500

    added favicon to reduce 404's in prep for fail2ban

--- flaskblog/main/views.py
+++ flaskblog/main/views.py
@@ -1,6 +1,7 @@
 
 
-from flask import Blueprint,render_template
+import os
+from flask import Blueprint,render_template,send_from_directory,current_app
 
 main = Blueprint('main',__name__)
 
@@ -10,6 +11,10 @@ main = Blueprint('main',__name__)
 def Home():
     return render_template('home.html', title = 'Home')
 
+@main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @main.route("/about")
 def About():
